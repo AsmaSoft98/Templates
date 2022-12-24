c93c4896dc122a5c001f1f46a6cc2db0e6ad3a9e
Amna@Soft <12900826+AsmaSoft98@users.noreply.github.com>
2022-12-24 17:46:31 +0100

    Add files via upload

---
+++ Template.API/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Template.Application.Services;
+using Template.Contracts.Authentication;
+
+namespace Template.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        
+        }
+
+
+        [HttpPost("register")]
+        public IActionResult Register(RegisterRequest request)
+        {
+            var authResult = _authService.Register(
+                request.FirstName,
+                request.LastName,
+                request.Email,
+                request.Password);
+            var response = new AuthResponse
+                (
+                authResult.Id,
+                authResult.FirstName,
+                authResult.LastName,
+                authResult.Email,
+                authResult.Token
+                );
+            return Ok(response);
+        }
+        [HttpPost("login")]
+        public IActionResult Login(LoginRequest request)
+        {
+            var authResult = _authService.Login(
+                request.Email,
+                request.Password);
+            var response = new AuthResponse
+                (
+                authResult.Id,
+                authResult.FirstName,
+                authResult.LastName,
+                authResult.Email,
+                authResult.Token
+                );
+            return Ok(response);
+        }
+    }
+}
